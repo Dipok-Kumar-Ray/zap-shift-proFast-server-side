ab290a49ac732fc36d08ebfc2078af6affef65ac
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-28 23:37:04 +0600

    Added Post data implementation

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 dotenv.config();
 
@@ -10,11 +11,58 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('Percel Server is running')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oclat4d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.listen(port, ()=> {
-    console.log(`Server is Listenign on port : ${port}`);
-})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("parcelDB"); // database name
+    const parcelCollection = db.collection("parcels"); // collection
+
+    // //parcels api
+    // app.get('/parcels', async(req, res)=> {
+    //     const userEmail = req.boi
+    // })
+
+    //POST: Create a new parcel
+    app.post("/parcels", async (req, res) => {
+      try {
+        const newparcel = req.body;
+        const result = await parcelCollection.insertOne(newparcel);
+        res.status(201).send(result);
+      } catch (error) {
+        console.error("Error inserting Parcel: ", error);
+        res.status(500).send({ message: "Failed to create parcel" });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Percel Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is Listening on port : ${port}`);
+});
